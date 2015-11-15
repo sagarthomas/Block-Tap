eb91af650af474f382f696e9d088c39c7584e4db
Sagar <sagarthomas27@hotmail.com>
2015-11-14 22:46:43 -0500

    Game Stage Update

--- core/src/com/unlimitedstudios/blocktap/stages/GameStage.java
+++ core/src/com/unlimitedstudios/blocktap/stages/GameStage.java
@@ -11,14 +11,36 @@ public class GameStage extends Stage{
     private static final int VIEWPORT_WIDTH = 13;
     private static final int VIEWPORT_HEIGHT = 20;
 
+
+
     private final float TIME_STEP = 1 / 300f;
     private float accumulator = 0f;
 
     private OrthographicCamera camera;
     //private Box2DDebugRenderer renderer
-    // guuuuuuuuyyyyyyys thaunks
-    // YAWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
 
+    public GameStage() {
+        setUpCamera();
+    }
+
+    private void setUpCamera() {
+        camera = new OrthographicCamera(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0f);
+        camera.update();
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        //fixed timestep
+       // accumulator += delta;
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
 
 
 }
---
+++ core/src/com/unlimitedstudios/blocktap/utils/WorldUtils.java
@@ -0,0 +1,9 @@
+package com.unlimitedstudios.blocktap.utils;
+
+/**
+ * Created by Sagar on 2015-11-14.
+ */
+public class WorldUtils {
+
+
+}
